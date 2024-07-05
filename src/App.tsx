62fcec6b4da15214ef07a0bcc6e8506e97131b14
Param <param23403@gmail.com>
2024-07-04 22:45:07 -0400

    Added NotFound page and cleaned up repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import JoinPage from './screens/JoinPage';
+import { Route, Routes } from "react-router-dom";
+import JoinPage from "./screens/JoinPage";
+import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path='/join' element={<JoinPage/>}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/join" element={<JoinPage />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 }
