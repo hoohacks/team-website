61a8335d5e23f14f47e374206863d2fbe0a1ddc2
Elnora <elnoramagruder@gmail.com>
2024-07-15 18:12:04 -0400

    styling alumni

--- src/screens/Home/Alumni.tsx
+++ src/screens/Home/Alumni.tsx
@@ -1,15 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import '../../Alumni.css';
 
 const Alumni = () => {
+    const [selectedYear, setSelectedYear] = useState(0);
+
+    const handleBarClick = (year: number)=>{
+      setSelectedYear(selectedYear === year ? 0 : year);
+    }
+
+    const eventsData = [
+        { year: 2020, events: ["Event 1 description", "Event 2 description"] },
+        { year: 2021, events: ["Event 3 description"] },
+        { year: 2022, events: ["Event 4 description", "Event 5 description", "Event 6 description"] },
+      ];
+
     return (
         <div>
-            <h2
-            style={{ color: "#121A6A" }}
-            className="font-bold text-2xl md:text-3xl lg:text-4xl text-center mb-8"
-        >
-            Our Alumni
-        </h2>
-        <p className="flex flex-col items-center md:flex-row-reverse md:justify-center"> Where are they now? </p>
+            <div>
+                <h2
+                    style={{ color: "#121A6A" }}
+                    className="font-bold text-2xl md:text-3xl lg:text-4xl text-center mb-8"
+                >
+                    Our Alumni
+                </h2>
+                <p className="flex flex-col items-center md:flex-row-reverse md:justify-center"> Where are they now? </p>
+            </div>
+
+            <div className="timeline">
+                <div className="bars">
+                    {eventsData.map((item, index)=> (
+                        <div>
+                            <div
+                                key={index}
+                                className={`bar ${selectedYear === item.year ? 'active' : ''}`}
+                                style={{height: selectedYear === item.year ? '80%' : '20%'}}
+                                onClick={() => handleBarClick(item.year)}
+                            >
+                                {item.year}
+                            </div>
+                            {selectedYear === item.year ? (
+                                <div>
+                                    <h3>
+                                    {item.events.map((event, index) => (
+                                    <h3 key={index}>{event}</h3>
+                                ))}
+                                    </h3>
+                                </div>
+                            ) : null }
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     );
 }
