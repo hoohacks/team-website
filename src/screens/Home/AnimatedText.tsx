57bd23482c6c16f2f5ef4d3d5f103d9489efc1d0
Varun Reddy <varun04reddy@gmail.com>
2024-08-24 16:19:49 -0400

    archive page done - varun

--- src/screens/Home/AnimatedText.tsx
+++ src/screens/Home/AnimatedText.tsx
@@ -3,12 +3,13 @@ import hoohacksLogo from "../../assets/hooMain.png";
 
 const AnimatedText = () => {
   return (
-    <div className="relative flex justify-center items-center h-screen bg-white overflow-hidden pt-24 md:pt-32 lg:pt-48 px-4 md:px-8 lg:px-16">
+    <div className="relative flex justify-center items-start h-screen bg-white overflow-hidden px-4 md:px-8 lg:px-16">
       <img
         src={hoohacksLogo}
         alt="HooHacks Logo"
-        className="z-20 h-24 md:h-32 lg:h-48"
+        className="z-20 h-24 md:h-32 lg:h-48 absolute top-12 md:top-8 lg:top-60"
       />
+      
     </div>
   );
 };
