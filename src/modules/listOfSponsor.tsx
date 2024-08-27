669ad66fa9c49cb1193ab1bbb0ac88d902f17654
Akhil-prog-design <akhilrdesai@gmail.com>
2024-08-27 18:42:38 -0400

    directory changes

--- src/modules/listOfSponsor.tsx
+++ src/modules/listOfSponsor.tsx
@@ -1,16 +1,16 @@
-import nvidiaLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/nvidia.svg';
-import appleLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/apple-black-logo-svgrepo-com.svg'
-import googleLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/google-icon-logo-svgrepo-com.svg';
-import ibmLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/ibm-logo-svgrepo-com.svg';
-import amazonLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/amazon-icon-logo-svgrepo-com.svg';
-import appianLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/appian-rebrand-1.svg'
-import geicoLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/geico-1.svg'
-import microsoftLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/microsoft-logo-svgrepo-com.svg'
-import githubLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/github-2.svg'
-import leidosLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/leidos-1.svg'
-import palantirLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/palantir-technologies-logo.svg'
-import redbullLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/redbull-logo-svgrepo-com.svg'
-import mongodbLogo from '/Users/akhil/Desktop/Computer Science /HooHacks/team-website/src/screens/sponsors/mongodb-icon-2.svg'
+import nvidiaLogo from '../screens/sponsors/nvidia.svg';
+import appleLogo from '../screens/sponsors/apple-black-logo-svgrepo-com.svg'
+import googleLogo from '../screens/sponsors/google-icon-logo-svgrepo-com.svg';
+import ibmLogo from '../screens/sponsors/ibm-logo-svgrepo-com.svg';
+import amazonLogo from '../screens/sponsors/amazon-icon-logo-svgrepo-com.svg';
+import appianLogo from '../screens/sponsors/appian-rebrand-1.svg'
+import geicoLogo from '../screens/sponsors/geico-1.svg'
+import microsoftLogo from '../screens/sponsors/microsoft-logo-svgrepo-com.svg'
+import githubLogo from '../screens/sponsors/github-2.svg'
+import leidosLogo from '../screens/sponsors/leidos-1.svg'
+import palantirLogo from '../screens/sponsors/palantir-technologies-logo.svg'
+import redbullLogo from '../screens/sponsors/redbull-logo-svgrepo-com.svg'
+import mongodbLogo from '../screens/sponsors/mongodb-icon-2.svg'
 
 export const sponsors = [
     {
